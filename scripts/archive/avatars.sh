cc107eaf7e6dc17054eab1bd440c5bf125fe750b
Phillip Pollard <phil@crescendo.net>
2014-01-02 23:13:11 -0800

    Correcting directories and running a backup.

--- scripts/archive/avatars.sh
+++ scripts/archive/avatars.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd /home/httpd/html/macrophile.com/scripts/archive
+cd /var/www/macrophile.com/scripts/archive
 mv avatars.tar.bz2 avatars.tar.bz2.old
-cd /home/httpd/html/macrophile.com/forums/images
+cd /var/www/macrophile.com/forums/images
 tar cvf ../../scripts/archive/avatars.tar avatars
-cd /home/httpd/html/macrophile.com/scripts/archive
+cd /var/www/macrophile.com/scripts/archive
 bzip2 -vv avatars.tar
