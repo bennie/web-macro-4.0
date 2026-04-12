e45dbc4203e484913285637c5c060274a736be6c
Phillip Pollard <phil@crescendo.net>
2026-04-11 21:13:05 -0400

    Moving live.

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 # Publish the combined site layout on to the server
 rsync -av --exclude='.git/' --exclude='.DS_Store' --exclude='*.swp' --delete lib/ ${MY_WEBUSER}@${MY_WEBHOST}:/var/www/macrophile.com/lib
 rsync -av --exclude='.git/' --exclude='.DS_Store' --exclude='*.swp' --delete www/ ${MY_WEBUSER}@${MY_WEBHOST}:/var/www/macrophile.com/dev
+rsync -av --exclude='.git/' --exclude='.DS_Store' --exclude='*.swp' --delete www/ ${MY_WEBUSER}@${MY_WEBHOST}:/var/www/macrophile.com/www
